7148e45e6fea252a761283079fabca58588b7c63
Samuel Nihoul <69475809+samuelnihoul@users.noreply.github.com>
2023-03-28 12:48:19 +0200

    converting to grid

--- pages/anomalyMap.tsx
+++ pages/anomalyMap.tsx
@@ -4,12 +4,12 @@
 import HeaderOne from '../components/Header/HeaderOne';
 import FooterOne from '../components/Footer/FooterOne';
 import Loader from '../components/Loader/Loader';
+import styles from './anomalyMap.module.css'
 export default function News() {
-
     return (
         <Loader>
             <HeaderOne type={undefined} />
-            <section style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around', paddingTop: '10%' }}>
+            <section className={styles.imageContainer}>
                 <img src='https://climatereanalyzer.org/wx/todays-weather/input/gfs_nh-sat1_t2anom_d1.png'>
                 </img>
                 <img src='https://climatereanalyzer.org/wx/todays-weather/input/gfs_euroafr-sat_t2anom_d1.png'>
