443fdca60292ee97c71a7ab2201f3d46e61ce0d6
Samuel Nihoul <69475809+samuelnihoul@users.noreply.github.com>
2023-08-19 21:27:47 +0200

    next -> vercel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import '../css/style.css'
 import "bootstrap/dist/css/bootstrap.min.css";
 import '../public/icofont/icofont.min.css'
-import { Analytics } from '@next/analtics'
+import { Analytics } from '@vercel/analytics'
 export default function App({ Component, pageProps }) {
  return (<><Analytics />
   <Component className="App" {...pageProps} /></>)
