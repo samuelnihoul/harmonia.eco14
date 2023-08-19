9e7874dbde95152b1f41183f33aba57436aecf7f
Samuel Nihoul <69475809+samuelnihoul@users.noreply.github.com>
2023-08-19 14:55:00 +0200

    verecel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,9 @@
-import React  from "react";
+import React from "react";
 import '../css/style.css'
 import "bootstrap/dist/css/bootstrap.min.css";
 import '../public/icofont/icofont.min.css'
-
+import { Analytics } from '@next/analtics'
 export default function App({ Component, pageProps }) {
-   return (<>
-    <Component className="App" {...pageProps} /></>)
+ return (<><Analytics />
+  <Component className="App" {...pageProps} /></>)
 }
