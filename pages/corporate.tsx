0d36a1f046a1b718120f367f4476703dd912664f
Samuel Nihoul <samuelnihoul@tutanota.com>
2023-11-27 08:25:34 +0100

    bun lock

---
+++ pages/corporate.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Corporate = () => {
+    return (
+        <>
+            <h1>Corporate Services</h1>
+            <ul className=''>
+                <li>
+                    Carbon Markets Consulting
+                </li>
+                <li>
+                    Sustainability and Impact Consulting
+                </li>
+                <li>
+                    Carbon Offset Management
+                </li>
+                <li>
+                    Carbon Accounting
+                </li>
+                <li>
+                    "1% for climate" Certifications and Solutions, Including Communication and Marketing Solutions.
+                </li>
+            </ul>
+        </>
+    )
+}
+export default Corporate 
