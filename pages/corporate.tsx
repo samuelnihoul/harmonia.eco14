1b5d58a93d20fd1bbc6f8e4a9874d9efffea77a8
Samuel Nihoul <samuelnihoul@tutanota.com>
2023-11-27 08:41:30 +0100

    contact us

--- pages/corporate.tsx
+++ pages/corporate.tsx
@@ -6,21 +6,22 @@ const Corporate = () => {
             <h1>Corporate Services</h1>
             <ul className=''>
                 <li>
-                    Carbon Markets Consulting
+                    - Carbon Markets Consulting
                 </li>
                 <li>
-                    Sustainability and Impact Consulting
+                    - Sustainability and Impact Consulting
                 </li>
                 <li>
-                    Carbon Offset Management
+                    - Carbon Offset Management
                 </li>
                 <li>
-                    Carbon Accounting
+                    - Carbon Accounting
                 </li>
                 <li>
-                    "1% for climate" Certifications and Solutions, Including Communication and Marketing Solutions.
+                    - "1% for climate" Certifications and Solutions, Including Communication and Marketing Solutions.
                 </li>
             </ul>
+            <p>Contact us at contact@harmonia.eco</p>
         </>
     )
 }
